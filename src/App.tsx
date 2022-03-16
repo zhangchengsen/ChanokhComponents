8119d20c658295c69f5c8c9d7e1ef9f8f76ff34c
张铖森 <1393908555@qq.com>
2022-03-16 15:21:18 +0800

    纯手写Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import React,{useState} from 'react';
-import './App.css';
-import Button,{ButtonSize,ButtonType} from './components/button'; 
+import React, { useState } from "react";
+import "./App.css";
+import Alert, { AlertType } from "./components/Alert";
+import Button, { ButtonSize, ButtonType } from "./components/button";
 
 function App() {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
   const changeShow = () => {
-    setShow(false)
-    setTimeout(()=>{
-      setShow(true)
-    },2000)
-  }
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 2000);
+  };
   return (
     <div className="App">
+      <Alert title="Chanokh_apt Alert~">Alert组件展示</Alert>
+      <Alert type={AlertType.Info}></Alert>
+      <Alert
+        type={AlertType.Danger}
+        onClose={() => alert("我被关闭了")}
+        description="关闭我还能多一个弹窗！"
+      ></Alert>
+      <Alert type={AlertType.Default}></Alert>
+      <Alert type={AlertType.Success}></Alert>
+      <Alert duration={1000}></Alert>
       <h2>Chanokh_apt Button</h2>
       <Button>Button</Button>
       <Button autoFocus>AutoFocus Button</Button>
-      <Button disabled >Button</Button>
-      {show && <Button  onClick ={()=>changeShow()} >Click Button</Button>}
-      
+      <Button disabled>Button</Button>
+      {show && <Button onClick={() => changeShow()}>Click Button</Button>}
       <Button size={ButtonSize.Large}>Button Large</Button>
       <Button size={ButtonSize.Small}>Button Small</Button>
       <Button btnType={ButtonType.Primary}>Button Primary</Button>
       <Button btnType={ButtonType.Danger}>Button Danger</Button>
       <Button btnType={ButtonType.Default}>Button Default</Button>
       <Button btnType={ButtonType.Link}>Button Link</Button>
-      <Button btnType={ButtonType.Link} href="http://wechatweb.ymtx.xyz">Button Link Redirect</Button>
-      <Button btnType={ButtonType.Link} disabled>Link Disabled</Button>
+      <Button btnType={ButtonType.Link} href="http://wechatweb.ymtx.xyz">
+        Button Link Redirect
+      </Button>
+      <Button btnType={ButtonType.Link} disabled>
+        Link Disabled
+      </Button>
     </div>
   );
 }
---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,99 @@
+import React, { memo, useState, useEffect } from "react";
+import classNames from "classnames";
+import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
+export const enum AlertType {
+  Danger = "danger",
+  Primary = "primary",
+  Info = "info",
+  Success = "success",
+  Default = "default",
+}
+interface IAlert {
+  type?: string;
+  title?: string;
+  description?: string;
+  top?: string;
+  duration?: number;
+  onClose?: () => void;
+}
+type PAlert = IAlert & React.HTMLAttributes<HTMLElement>;
+const Alert: React.FC<PAlert> = memo((props) => {
+  const {
+    type,
+    title,
+    description,
+    className,
+    onClose,
+    top,
+    duration,
+    ...restProps
+  } = props;
+  const [show, setState] = useState(true);
+  let [target, setTarget] = useState<HTMLElement | null>(null);
+  const close = () => {
+    if (!duration) {
+      setState(false);
+    }
+    onClose!();
+  };
+  useEffect(() => {
+    if (duration) {
+      setTimeout(() => {
+        setState(false);
+      }, duration);
+    }
+  }, []);
+  const classes = classNames(className, {
+    [`Alert-${type}`]: type,
+  });
+  useEffect(() => {
+    let preTar = document.getElementById("alert_div");
+    if (!preTar) {
+      let popupMaskNode = document.createElement("div");
+      popupMaskNode.setAttribute("id", "alert_div");
+      popupMaskNode.setAttribute("class", "position-fixed top-0 left-center");
+      document.body.appendChild(popupMaskNode);
+    }
+
+    let tar = document.getElementById("alert_div");
+    setTarget(tar);
+  }, []);
+  return target ? (
+    createPortal(
+      <CSSTransition
+        timeout={500}
+        classNames="Alert"
+        unmountOnExit={true}
+        appear
+        in={show}
+      >
+        <div className="m-2 ">
+          <div className={classes} style={{ top }} {...restProps}>
+            <div className="title-wrap">
+              <div className="title">{title}</div>
+              <div className="close" onClick={() => close()}>
+                {duration ? "" : "x"}
+              </div>
+            </div>
+            <div className="description">
+              {description || restProps.children}
+            </div>
+          </div>
+        </div>
+      </CSSTransition>,
+      target
+    )
+  ) : (
+    <div></div>
+  );
+});
+Alert.defaultProps = {
+  title: "This is an Alert",
+  type: AlertType.Primary,
+  onClose: () => {
+    return;
+  },
+  top: "10%",
+};
+export default Alert;
